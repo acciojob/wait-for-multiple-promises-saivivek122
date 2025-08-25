20cbd82ec36fc26b4fe80bbf1cd002d146e86e6a
saivivek122 <119473212+saivivek122@users.noreply.github.com>
2025-08-25 17:35:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,51 +1,33 @@
 let tableBody=document.getElementById("output");
 let loadingRow=document.getElementById("loading")
 
-let promise1=new Promise((resolve,reject)=>{
-    setTimeout(()=>{
-        resolve("1")
-    },1000)
-})
+function calculateTime(name,delay){
+	let startTime=Date.now();
+	return new Promise(resolve=>{
+		setTimeout(()=>{
+			let endTime=Date.now();
+			let totalTime=Math.round((endTime-startTime)/1000)
+			resolve({name,totalTime})
+		},delay)
+	})
+}
 
-let promise2=new Promise((resolve,reject)=>{
-    setTimeout(()=>{
-        resolve("2")
-    },2000)
-})
-
-let promise3=new Promise((resolve,reject)=>{
-    setTimeout(()=>{
-        resolve("3")
-    },3000)
-})
+let promise1=calculateTime("Promise 1",1000);
+let promise2=calculateTime("Promise 2",2000);
+let promise3=calculateTime("Promise 3",3000);
 
 Promise.all([promise1,promise2,promise3]).then((data)=>{
-  loadingRow.remove()
-
-    let row1=tableBody.insertRow(0);
-    let row1Cell1=row1.insertCell(0);
-    let row1Cell2=row1.insertCell(1);
-    row1Cell1.innerHTML="Promise 1";
-    row1Cell2.innerHTML=data[0];
-    
-    let row2=tableBody.insertRow(1);
-    let row2Cell1=row2.insertCell(0);
-    let row2Cell2=row2.insertCell(1);
-    row2Cell1.innerHTML="Promise 2";
-    row2Cell2.innerHTML=data[1];
-
-    let row3=tableBody.insertRow(2);
-    let row3Cell1=row3.insertCell(0);
-    let row3Cell2=row3.insertCell(1);
-    row3Cell1.innerHTML="Promise 3";
-    row3Cell2.innerHTML=data[2];
-
-    let row4=tableBody.insertRow(3);
-    let row4Cell1=row4.insertCell(0);
-    let row4Cell2=row4.insertCell(1);
-    row4Cell1.innerHTML="Total";
-    row4Cell2.innerHTML=Number(data[0])+Number(data[1])+Number(data[2]);
-    // console.log(typeof data[0])
-    // console.log(typeof data[1])
-    // console.log(typeof data[2])
-});
+	loadingRow.remove()
+	data.forEach(currentData=>{
+		let row=tableBody.insertRow();
+		row.insertCell(0).innerHTML=currentData.name;
+		row.insertCell(1).innerHTML=currentData.totalTime
+	})
+	let sum=0;
+	data.forEach(currentData=>{
+		sum=sum+currentData.totalTime;
+	})
+	let totalRow=tableBody.insertRow();
+	totalRow.insertCell(0).innerHTML="Total";
+	totalRow.insertCell(1).innerHTML=sum;
+})
